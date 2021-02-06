12de0ce1cc089294d1307c1b0cb81b015632fc80
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2021-02-06 22:12:44 +0000

    Add CORS

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let express = require('express')
 let bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 let app = express();
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 const uri = process.env.URI;
 
+app.use(cors());
+
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {console.log('MongoDB Connected…')})
+    .then(() => {
+        console.log('MongoDB Connected…')
+    })
     .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,8 +20,8 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Henlo'));
 
-app.listen(port, function() {
-  console.log('Henlo from ' + port);
+app.listen(port, function () {
+    console.log('Henlo from ' + port);
 });
 
 let apiRoutes = require('./api-routes')
